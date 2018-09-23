7a257cfa3bd4ec3cca36ac679c1b71c45f5fbcce
RadiBarq <grayllow@gmail.com>
2018-09-23 10:28:48 +0300

    working

---
+++ NasaInArabic/ArticleTableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ArticleTableViewController.swift
+//  NasaInArabic
+//
+//  Created by Radi Barq on 9/21/18.
+//  Copyright © 2018 RadiBarq. All rights reserved.
+//
+
+import UIKit
+
+class ArticleTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
